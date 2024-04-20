774b1a3e445649d494e0c2993c7ac8ee6b9bce1c
Rajithxx <148357145+Rajithxx@users.noreply.github.com>
2024-04-20 13:04:40 +0530

    212221060222 encoder8to3.v
    Rajith Rufus

--- encoder8to3.srcs/sources_1/new/encoder8to3.v
+++ encoder8to3.srcs/sources_1/new/encoder8to3.v
@@ -1,30 +1,17 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.01.2024 13:55:06
-// Design Name: 
-// Module Name: encoder8to3
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module encoder8to3(a0,a1,a2,d0,d1,d2,d3,d4,d5,d6,d7);
-input d0,d1,d2,d3,d4,d5,d6,d7;
-output a0,a1,a2;
-                     //Write Verilog code for 8to3 encoder using dataflow modeling
-
-
+module encwtoutprio(d,q);
+input [7:0] d;
+output reg [2:0] q;
+always@(*)
+begin
+case(d)
+8'b00000001:q<=3'b000;
+8'b00000010:q<=3'b001;
+8'b00000100:q<=3'b010;
+8'b00001000:q<=3'b011;
+8'b00010000:q<=3'b100;
+8'b00100000:q<=3'b101;
+8'b01000000:q<=3'b110;
+8'b10000000:q<=3'b111;
+endcase
+end
 endmodule
-
